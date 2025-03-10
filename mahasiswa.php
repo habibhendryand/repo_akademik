e8abaf06215c6bc44c30a591029062dd53e61c77
habibhendryand <133472516+habibhendryand@users.noreply.github.com>
2025-03-10 11:47:09 +0700

    Create mahasiswa.php
    Buat Modul Mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+
+<body>
+    <h6>Data Mahasiswa AMP</h6>
+    <hr>
+    <h6>NIM : 23110020</h6><br>
+    <h6>NAMA : Habieb Hendryand</h6><br>
+    <a href="index.php">Kembali</a>
+</body>
+
+</html>
